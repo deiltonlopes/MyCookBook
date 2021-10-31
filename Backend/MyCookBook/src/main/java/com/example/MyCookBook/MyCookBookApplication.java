76aaba167ea63a94ecfc9ddfefacc6e035f0fd3b
deiltonlopes <deilton.lopes@ccc.ufcg.edu.br>
2021-10-30 23:42:23 -0300

    Starting the spring boot project for the backend

---
+++ Backend/MyCookBook/src/main/java/com/example/MyCookBook/MyCookBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyCookBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCookBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCookBookApplication.class, args);
+	}
+
+}
---
+++ Backend/MyCookBook/src/test/java/com/example/MyCookBook/MyCookBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyCookBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCookBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
