d946270d1218122c48c0f4c254a45f50f11dfbb9
deiltonlopes <deilton.lopes@ccc.ufcg.edu.br>
2021-11-05 17:20:43 -0300

    Starting to code index

--- Frontend/scripts/script.js
+++ Frontend/scripts/script.js
@@ -1,12 +1,19 @@
-function getMaker(url){
+function bodylessRequestMaker(method, url){
     let request = new XMLHttpRequest
-    request.open('GET', url, false)
+    request.open(method, url, false)
     request.send()
     return request.responseText
 }
 
+function requestMaker(method, url, body){
+    let request = new XMLHttpRequest
+    request.open(method, url, false)
+    request.send(body)
+    return request.responseText
+}
+
 function getListCategory(category){
-    data = getMaker(`https://my-cook-book-bck.herokuapp.com/recipes/category/${category}`)
-    recipesList = JSON.parse
+    data = bodylessRequestMaker('GET', `https://my-cook-book-bck.herokuapp.com/api/recipes/category/${category}`)
+    recipesList = JSON.parse(data)
     console.log(recipesList)
 }
